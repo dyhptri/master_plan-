94b7e762d5b39c36135931a92c59c0fa9a0062a0
dyhptri <diyahrp10@gmail.com>
2026-05-06 14:02:45 +0700

    Praktikum 3

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './views/plan_screen.dart';
 import './provider/plan_provider.dart';
 import './models/data_layer.dart';
+import './views/plan_creator_screen.dart';
 
 void main() => runApp(MasterPlanApp());
 
@@ -10,11 +11,12 @@ class MasterPlanApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple, useMaterial3: false),
-      home: PlanProvider(
-        notifier: ValueNotifier<Plan>(const Plan()),
-        child: const PlanScreen(),
+    return PlanProvider(
+      notifier: ValueNotifier<List<Plan>>(const []),
+      child: MaterialApp(
+        title: 'State management app',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const PlanCreatorScreen(),
       ),
     );
   }
